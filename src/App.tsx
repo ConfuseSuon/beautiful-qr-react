ec4e6d2643ce157e26bdf1f863cd44c717624354
ConfuseSuon <sonupun500@gmail.com>
2024-05-10 12:23:58 +0545

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react/jsx-runtime";
+import QrScanner from "./QrScanner";
+
+const App = () => {
+  return (
+    <Fragment>
+      <QrScanner />
+    </Fragment>
+  );
+};
+
+export default App;
